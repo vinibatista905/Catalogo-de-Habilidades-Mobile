7949fd140be13f784b5d20776acf14e1a24885ea
Vinicius Batista <vinicius-moura-batista@hotmail.com>
2021-11-24 12:13:58 -0300

    Instalando dependencias

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Register from './screens/Register'
+import Login from './screens/login'
+
+
+function routes() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default routes
---
+++ src/screens/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "react-native";
+
+function Login(params) {
+    return(
+        <Text>Tela de Login!</Text>
+    )
+}
+
+export default Login;
