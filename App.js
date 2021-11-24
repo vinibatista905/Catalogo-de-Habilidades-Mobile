983187471c69b2dbab968b7b86c175e989a9217e
Vinicius Batista <vinicius-moura-batista@hotmail.com>
2021-11-24 11:26:56 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Register from './src/screens/Register';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Register />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/Register/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "react-native";
+
+function Register(params) {
+    return(
+        <Text>Tela de Registro</Text>
+    )
+}
+
+export default Register;
